5efe02e24abd04c8de91e96150623857eebfbf43
vivekkar88 <136933939+vivekkar88@users.noreply.github.com>
2023-07-20 22:49:21 +0530

    Update vending machine design.v

--- vending machine design.v
+++ vending machine design.v
@@ -10,69 +10,69 @@ module vending_machine(clk,rst,x,out,change);
     begin
      if(rst)
       begin
-      ps<=2'b00;
-      ns<=2'b00;
-      change<=2'b00;
+      ps=2'b00;
+      ns=2'b00;
+      change=2'b00;
       end
      else
-       ps<=ns;
+       ps=ns;
       case(ps)
         s0:
           if(x==0)
             begin 
-              ns<=s0;
-              out<=0;
+              ns=s0;
+              out=0;
               change=2'b00;
             end
         else if(x==2'b01)
              begin
-               ns<=s1;
-               out<=0;
-               change<=2'b00;
+               ns=s1;
+               out=0;
+               change=2'b00;
              end
         else if(x==2'b10)
              begin
-               ns<=s2;
-               out<=0;
-               change<=2'b00;
+               ns=s2;
+               out=0;
+               change=2'b00;
              end
         s1:
           if(x==0)
             begin
-              ns<=s0;
-              out<=0;
-              change<=2'b01;
+              ns=s0;
+              out=0;
+              change=2'b01;
             end
         else if(x==2'b01)
             begin
-              ns<=s2;
-              out<=0;
-              change<=2'b00;
+              ns=s2;
+              out=0;
+              change=2'b00;
             end
         else if(x==2'b10)
             begin
-              ns<=s0;
-              out<=1;
-              change<=2'b00;
+              ns=s0;
+              out=1;
+              change=2'b00;
             end
         s2:
           if(x==0)
             begin
-              ns<=s0;
-              out<=0;
-              change<=2'b10;
+              ns=s0;
+              out=0;
+              change=2'b10;
             end
         else if(x==2'b01)
             begin
-              ns<=s0;
-              out<=1;
-              change<=2'b00;
+              ns=s0;
+              out=1;
+              change=2'b00;
             end
         else if(x==2'b10)
             begin
-              ns<=s0;
-              out<=1;
-              change<=2'b01;
+              ns=s0;
+              out=1;
+              change=2'b01;
             end
       endcase
     end
